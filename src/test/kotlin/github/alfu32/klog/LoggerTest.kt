1c9d7b00763387373ba450310a56eb93ad0c4748
Ioan ALferaru <ioan.alferaru@gmail.com>
2022-07-18 06:32:04 +0000

    implementation
    added two tests

--- src/test/kotlin/github/alfu32/klog/LoggerTest.kt
+++ src/test/kotlin/github/alfu32/klog/LoggerTest.kt
@@ -3,11 +3,20 @@ package github.alfu32.klog
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-internal class SampleTest {
+internal class LoggerTest {
 
     @Test
-    fun testSum() {
+    fun testRun01() {
+        println("test run 01")
         val expected = 42
         assertEquals(expected, 40 + 2 )
+
+    }
+    @Test
+    fun testRun02() {
+        println("test run 02")
+        val expected = 42
+        assertEquals(expected, 40 + 2 )
+
     }
 }
