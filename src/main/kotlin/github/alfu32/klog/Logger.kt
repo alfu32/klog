91ba11ce3b5525394a6a18c1eb15108ef421e006
Ioan ALferaru <ioan.alferaru@gmail.com>
2022-07-18 06:11:24 +0000

    implementation

--- src/main/kotlin/github/alfu32/klog/Logger.kt
+++ src/main/kotlin/github/alfu32/klog/Logger.kt
@@ -1,34 +1,52 @@
 package github.alfu32.klog
 
 import java.io.PrintWriter
-interface Stringifiable{
-    override fun toString(): String
-}
-class Logger{
-    public fun log(channelName: String, message: Stringifiable){
 
-    }
+class Logger{
+  public fun log(channelName: String, message: Object){
+    val lm = LogMessage(
+      channelName=channelName,
+      message=message,
+    )
+    LoggerMessageBroadcaster.emit(lm)
+  }
 }
 
 class LoggerMessageBroadcaster(
-    
+  
 ){
-    companion object{
-        var subscribers = arrayListOf<LogMessageListener>()
-
+  companion object{
+    var subscribers = arrayListOf<LogMessageListener>()
+    fun emit(lm:LogMessage){
+      subscribers.forEach{
+        s -> {
+          if(s.filter(lm)){
+            try{
+              val msg = s.logMessageToString(lm)
+              s.printWriter.println(msg)
+            }catch(x: Exception){
+              //DO NOTHING
+            }
+          }
+        }
+      }
     }
+  }
 }
 
 class LogMessageListener(
-    val printWriter: PrintWriter,
-    val filter: (LogMessage)->Boolean,
-    val logMessageToString: (LogMessage)->String,
+  val printWriter: PrintWriter,
+  val filter: (LogMessage)->Boolean,
+  val logMessageToString: (LogMessage)->String,
 )
 
-class LogMessage{
-    val place: StackTraceElement
-    init{
-        val p = Throwable()
-        place=p.getStackTrace()[1]
-    }
+class LogMessage(
+  val channelName: String,
+  val message:Object,
+){
+  val place: StackTraceElement
+  init{
+    val p = Throwable()
+    place=p.getStackTrace()[1]
+  }
 }
