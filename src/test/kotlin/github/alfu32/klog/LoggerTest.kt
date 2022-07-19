93b0e8b5c978b0073f4578bfe0dd751b9d2bdab9
Ioan ALferaru <ioan.alferaru@gmail.com>
2022-07-19 18:52:44 +0200

    Update LoggerTest.kt

--- src/test/kotlin/github/alfu32/klog/LoggerTest.kt
+++ src/test/kotlin/github/alfu32/klog/LoggerTest.kt
@@ -6,21 +6,26 @@ import kotlin.test.assertNull
 
 internal class LoggerTest {
 
-    @Test
-    fun testInit() {
-        println("=== test LoggerTest.testInit =========================")
-        val lm=LogMessage("test", "message")
-        println(lm)
-        assertEquals("test",lm.channelName)
-        assertEquals("message",lm.message)
-        assertEquals("LoggerTest.kt",lm.fileName)
-        assertEquals(12,lm.lineNumber)
-        assertNull(lm.moduleName)
-        assertNull(lm.moduleVersion)
-        assertEquals("app",lm.classLoaderName)
-        assertEquals("github.alfu32.klog.LoggerTest",lm.className)
-        assertEquals("testInit",lm.methodName)
-        println("=== done LoggerTest.testInit -------------------------")
+    @ Test
+    fun testSubscribe() {
+        val lmb=LoggerMessageBroadcaster
+        lmb.subscribe(listOf(
+            LogMessageListener(
+                name="console",
+                printWriter=PrintWriter(System.out, true),
+                filter={true},
+                logMessageToString={ it.toString() }
+            ),
+            LogMessageListener(
+                name="file",
+                printWriter=File("log").printWriter(),
+                filter={true},
+                logMessageToString={ it.toString() }
+            ),
+        ))
+        println(lmb.subscribers)
+        val l=Logger()
+        
     }
     @Test
     fun testRun02() {
