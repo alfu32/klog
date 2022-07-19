2d1a397dc152a7d1cace6c314b87ea2bf3b8ce47
ioan alferaru <ioan.alferaru@gmail.com>
2022-07-19 12:02:44 +0000

    implementation: LogMessage and Test

--- src/test/kotlin/github/alfu32/klog/LoggerMessageBroadcasterTest.kt
+++ src/test/kotlin/github/alfu32/klog/LoggerMessageBroadcasterTest.kt
@@ -2,15 +2,20 @@ package github.alfu32.klog
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import java.io.PrintWriter
 
 internal class LoggerMessageBroadcasterTest {
 
     @Test
-    fun testRun01() {
-        println("test run 01")
-        val expected = 42
-        assertEquals(expected, 40 + 2 )
-
+    fun testSubscribe() {
+        val lmb=LoggerMessageBroadcaster
+        lmb.subscribe(listOf(LogMessageListener(
+            name="general",
+            printWriter=PrintWriter(System.out, true),
+            filter={true},
+            logMessageToString={ it.toString() }
+        )))
+        println(lmb.subscribers)
     }
     @Test
     fun testRun02() {
