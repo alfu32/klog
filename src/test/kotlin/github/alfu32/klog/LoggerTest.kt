bb733a929a0d52391813dfc4f64e0547d378b4dd
ioan alferaru <ioan.alferaru@gmail.com>
2022-07-19 05:39:11 +0000

    implementation: LogMessage and Test

--- src/test/kotlin/github/alfu32/klog/LoggerTest.kt
+++ src/test/kotlin/github/alfu32/klog/LoggerTest.kt
@@ -2,15 +2,25 @@ package github.alfu32.klog
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 internal class LoggerTest {
 
     @Test
-    fun testRun01() {
-        println("test run 01")
-        val expected = 42
-        assertEquals(expected, 40 + 2 )
-
+    fun testInit() {
+        println("=== test LoggerTest.testInit =========================")
+        val lm=LogMessage("test", "message")
+        println(lm)
+        assertEquals("test",lm.channelName)
+        assertEquals("message",lm.message)
+        assertEquals("LoggerTest.kt",lm.fileName)
+        assertEquals(12,lm.lineNumber)
+        assertNull(lm.moduleName)
+        assertNull(lm.moduleVersion)
+        assertEquals("app",lm.classLoaderName)
+        assertEquals("github.alfu32.klog.LoggerTest",lm.className)
+        assertEquals("testInit",lm.methodName)
+        println("=== done LoggerTest.testInit -------------------------")
     }
     @Test
     fun testRun02() {
